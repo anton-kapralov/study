465246ae70737b32e3100e35de5c6e425e2c3576
Anton Kapralov <a.e.kapralov@gmail.com>
2019-10-10 20:19:40 -0700

    Replace JUnit 5 with JUnit 4.

--- coding-challenges/src/test/java/kae/study/codingchallenges/graphs/TestBreadthFirstSearch.java
+++ coding-challenges/src/test/java/kae/study/codingchallenges/graphs/TestBreadthFirstSearch.java
@@ -1,6 +1,6 @@
 package kae.study.codingchallenges.graphs;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.util.Arrays;
 
--- coding-challenges/src/test/java/kae/study/codingchallenges/graphs/TestBreadthFirstTraversal.java
+++ coding-challenges/src/test/java/kae/study/codingchallenges/graphs/TestBreadthFirstTraversal.java
@@ -1,55 +1,38 @@
 package kae.study.codingchallenges.graphs;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.google.common.truth.Truth.assertThat;
 
 class TestBreadthFirstTraversal {
 
   @Test
   void testTraverseTree() {
-    Node<Integer> root = new Node<>(
-        1,
-        Arrays.asList(
-            new Node<>(
-                2,
-                Arrays.asList(
-                    new Node<>(
-                        5,
-                        Arrays.asList(
-                            new Node<>(9),
-                            new Node<>(10)
-                        )
-                    ),
-                    new Node<>(6)
-                )
-            ),
-            new Node<>(3),
-            new Node<>(
-                4,
-                Arrays.asList(
-                    new Node<>(
-                        7,
-                        Arrays.asList(
-                            new Node<>(11),
-                            new Node<>(12)
-                        )
-                    ),
-                    new Node<>(8)
-                )
-            )
-        )
-    );
+    Node<Integer> root =
+        new Node<>(
+            1,
+            Arrays.asList(
+                new Node<>(
+                    2,
+                    Arrays.asList(
+                        new Node<>(5, Arrays.asList(new Node<>(9), new Node<>(10))),
+                        new Node<>(6))),
+                new Node<>(3),
+                new Node<>(
+                    4,
+                    Arrays.asList(
+                        new Node<>(7, Arrays.asList(new Node<>(11), new Node<>(12))),
+                        new Node<>(8)))));
 
     final List<Integer> result = new LinkedList<>();
     BreadthFirstTraversal.traverse(root, node -> result.add(node.getValue()));
 
     System.out.println(result);
-    assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), result);
+    assertThat(result).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
   }
 
   @Test
@@ -58,39 +41,19 @@ class TestBreadthFirstTraversal {
     final Node<Integer> tenth = new Node<>(10);
     final Node<Integer> third = new Node<>(3);
     final Node<Integer> eleventh = new Node<>(11);
-    Node<Integer> root = new Node<>(
-        1,
-        Arrays.asList(
-            new Node<>(
-                2,
-                Arrays.asList(
-                    new Node<>(
-                        5,
-                        Arrays.asList(
-                            ninthNode,
-                            tenth
-                        )
-                    ),
-                    new Node<>(6),
-                    third
-                )
-            ),
-            third,
-            new Node<>(
-                4,
-                Arrays.asList(
-                    new Node<>(
-                        7,
-                        Arrays.asList(
-                            eleventh,
-                            new Node<>(12)
-                        )
-                    ),
-                    new Node<>(8)
-                )
-            )
-        )
-    );
+    Node<Integer> root =
+        new Node<>(
+            1,
+            Arrays.asList(
+                new Node<>(
+                    2,
+                    Arrays.asList(
+                        new Node<>(5, Arrays.asList(ninthNode, tenth)), new Node<>(6), third)),
+                third,
+                new Node<>(
+                    4,
+                    Arrays.asList(
+                        new Node<>(7, Arrays.asList(eleventh, new Node<>(12))), new Node<>(8)))));
 
     ninthNode.getLinkedNodes().addAll(Arrays.asList(root, tenth));
     third.getLinkedNodes().add(eleventh);
@@ -100,7 +63,6 @@ class TestBreadthFirstTraversal {
     BreadthFirstTraversal.traverse(root, node -> result.add(node.getValue()));
 
     System.out.println(result);
-    assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 11, 7, 8, 9, 10, 12), result);
+    assertThat(result).isEqualTo(Arrays.asList(1, 2, 3, 4, 5, 6, 11, 7, 8, 9, 10, 12));
   }
-
 }
