f5b0c5927edb230f8800707034114e758bcefbb8
Anton Kapralov <a.e.kapralov@gmail.com>
2020-08-13 19:50:41 -0700

    IntRangeQuery

---
+++ coding-challenges/src/main/java/kae/study/codingchallenges/algorithms/arrays/IntRangeQuery.java
@@ -0,0 +1,70 @@
+package kae.study.codingchallenges.algorithms.arrays;
+
+import java.util.Arrays;
+import java.util.function.IntBinaryOperator;
+
+/** */
+public class IntRangeQuery {
+
+  private final IntBinaryOperator operator;
+  private final int identity;
+  private final int[] tree;
+  private final int n;
+
+  public IntRangeQuery(int[] nums, IntBinaryOperator operator, int identity) {
+    this.operator = operator;
+    this.identity = identity;
+
+    int sizePow2 = 1;
+    //noinspection StatementWithEmptyBody
+    for (; sizePow2 < nums.length; sizePow2 <<= 1)
+      ;
+
+    n = sizePow2;
+
+    this.tree = new int[2 * n];
+
+    System.arraycopy(nums, 0, tree, n, nums.length);
+    Arrays.fill(tree, n + nums.length, tree.length, identity);
+
+    for (int i = n - 1; i > 0; --i) {
+      tree[i] = operator.applyAsInt(tree[i * 2], tree[i * 2 + 1]);
+    }
+  }
+
+  public int get(int l, int r) {
+    l += n;
+    r += n;
+
+    int min = identity;
+
+    while (l <= r) {
+      if (l % 2 == 1) {
+        // l is right child => out of range, so adding to the result.
+        min = operator.applyAsInt(min, tree[l++]);
+      }
+
+      if (r % 2 == 0) {
+        // r is left child => out of range, so adding to the result.
+        min = operator.applyAsInt(min, tree[r--]);
+      }
+
+      l /= 2;
+      r /= 2;
+    }
+
+    return min;
+  }
+
+  public void update(int i, int v) {
+    i += n;
+
+    tree[i] = v;
+
+    i /= 2;
+    while (i > 0) {
+      tree[i] = operator.applyAsInt(tree[i * 2], tree[i * 2 + 1]);
+      i /= 2;
+    }
+  }
+}
---
+++ coding-challenges/src/test/java/kae/study/codingchallenges/algorithms/arrays/IntRangeQueryTest.java
@@ -0,0 +1,63 @@
+package kae.study.codingchallenges.algorithms.arrays;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class IntRangeQueryTest {
+
+  @Test
+  public void get_returnsGlobalMin() {
+    int[] nums = {4, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Math::min, Integer.MAX_VALUE);
+
+    assertThat(query.get(0, nums.length - 1)).isEqualTo(1);
+  }
+
+  @Test
+  public void get_returnsLocalMin() {
+    int[] nums = {0, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Math::min, Integer.MAX_VALUE);
+
+    assertThat(query.get(1, 3)).isEqualTo(1);
+  }
+
+  @Test
+  public void get_returnsLocalMax() {
+    int[] nums = {0, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Math::max, Integer.MIN_VALUE);
+
+    assertThat(query.get(1, 3)).isEqualTo(7);
+  }
+
+  @Test
+  public void get_returnsLocalSum() {
+    int[] nums = {0, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Integer::sum, 0);
+
+    assertThat(query.get(1, 3)).isEqualTo(11);
+  }
+
+  @Test
+  public void update_updatesTreeForGlobalQuery() {
+    int[] nums = {4, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Math::min, Integer.MAX_VALUE);
+
+    query.update(3, 8);
+
+    assertThat(query.get(0, nums.length - 1)).isEqualTo(3);
+  }
+
+  @Test
+  public void update_updatesTreeForLocalQuery() {
+    int[] nums = {0, 7, 3, 1, 5, 7};
+    IntRangeQuery query = new IntRangeQuery(nums, Math::min, Integer.MAX_VALUE);
+
+    query.update(3, 8);
+
+    assertThat(query.get(1, 3)).isEqualTo(3);
+  }
+}
