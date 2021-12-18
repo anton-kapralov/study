177367da55c61262d942ee933a850e91476ec32a
Anton Kapralov <a.e.kapralov@gmail.com>
2021-12-18 15:25:10 -0800

    [cses] Increasing Array
    You are given an array of n integers. You want to modify the array so that it is increasing, i.e., every element is at least as large as the previous element.
    
    On each move, you may increase the value of any element by one. What is the minimum number of moves required?

---
+++ coding-challenges/src/main/java/kae/study/codingchallenges/cses/problemset/introductory/IncreasingArray.java
@@ -0,0 +1,19 @@
+package kae.study.codingchallenges.cses.problemset.introductory;
+
+/** <a href="https://cses.fi/problemset/task/1094/">https://cses.fi/problemset/task/1094/</a> */
+public class IncreasingArray {
+  public static long solve(int[] arr) {
+    long k = 0;
+
+    for (int i = 1; i < arr.length; i++) {
+      int d = arr[i - 1] - arr[i];
+      if (d <= 0) {
+        continue;
+      }
+      k += d;
+      arr[i] = arr[i - 1];
+    }
+
+    return k;
+  }
+}
