50a84cc4a5985d159327cbd72b57e84fcc477b82
Anton Kapralov <a.e.kapralov@gmail.com>
2022-02-13 19:48:37 -0800

    [cses] Permutations
    A permutation of integers 1,2,…,n is called beautiful if there are no adjacent elements whose difference is 1.
    
    Given n, construct a beautiful permutation if such a permutation exists.

---
+++ coding-challenges/src/main/java/kae/study/codingchallenges/cses/problemset/introductory/Permutations.java
@@ -0,0 +1,29 @@
+package kae.study.codingchallenges.cses.problemset.introductory;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/** https://cses.fi/problemset/task/1070 */
+public class Permutations {
+  public static void solve(int n) throws IOException {
+    if (n == 1) {
+      System.out.println(1);
+      return;
+    }
+    if (n < 4) {
+      System.out.println("NO SOLUTION");
+      return;
+    }
+
+    try (OutputStream out = new BufferedOutputStream(System.out)) {
+      out.write("2".getBytes());
+      for (int i = 4; i <= n; i += 2) {
+        out.write((" " + i).getBytes());
+      }
+      for (int i = 1; i <= n; i += 2) {
+        out.write((" " + i).getBytes());
+      }
+    }
+  }
+}
