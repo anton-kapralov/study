f81c4d8d243ac86c1eaa68fe0b3a84fcc846037a
Anton Kapralov <a.e.kapralov@gmail.com>
2022-08-27 11:59:21 -0700

    [facebook/hackercup] Second Second Friend
    Boss Rob painted a beautiful scene on a 2D canvas of RR rows by CC columns, containing zero or more happy little trees and zero or more rocks.
    To make sure none of his trees are lonely, Rob would like you to add as many trees as you'd like (possibly 00) to empty spaces so that each tree in the final painting has at least two tree friends, that is, two trees which are each adjacent to it (directly to its north, south, east, or west). If there are multiple solutions, you may print any one of them.

---
+++ coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2022/qualification/SecondFriend2Test.java
@@ -0,0 +1,73 @@
+package kae.study.codingchallenges.facebook.hackercup2022.qualification;
+
+import static com.google.common.truth.Truth.assertThat;
+import static kae.study.codingchallenges.facebook.hackercup2022.qualification.SecondFriend2.solve;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value = Parameterized.class)
+public class SecondFriend2Test {
+
+  @Parameter
+  public char[][] input;
+
+  @Parameter(1)
+  public boolean expected;
+
+  @Parameters(name = "Test {index}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][]{
+            {new char[][]{{'.', '^', '.'}}, false},
+            {new char[][]{{'.'}, {'#'}, {'#'}}, true},
+            {new char[][]{{'^'}, {'#'}, {'#'}}, false},
+            {new char[][]{
+                {'.', '.'},
+                {'.', '#'},
+            }, true},
+            {new char[][]{
+                {'^', '.'},
+                {'.', '#'},
+            }, false},
+            {new char[][]{
+                {'.', '.'},
+                {'.', '.'},
+            }, true},
+            {new char[][]{
+                {'#', '#'},
+                {'#', '.'},
+            }, true},
+            {new char[][]{
+                {'#', '#'},
+                {'#', '^'},
+            }, false},
+            {new char[][]{
+                {'#', '.', '#'},
+                {'.', '^', '.'},
+                {'#', '.', '#'},
+            }, false},
+            {new char[][]{
+                {'#', '.', '#'},
+                {'.', '^', '.'},
+                {'.', '.', '#'},
+            }, true},
+            {new char[][]{
+                {'.', '.', '^', '.'},
+                {'.', '#', '^', '#'},
+                {'.', '.', '.', '.'},
+                {'.', '.', '.', '^'},
+            }, true},
+        });
+  }
+
+  @Test
+  public void solve_returnsExpected() {
+    assertThat(solve(input)).isEqualTo(expected);
+  }
+}
