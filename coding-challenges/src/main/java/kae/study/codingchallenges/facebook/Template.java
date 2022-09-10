c17401123595f4c70995ddb0e62d4b5bf40e1ae4
Anton Kapralov <a.e.kapralov@gmail.com>
2022-09-10 10:50:43 -0700

    [facebook/hackercup] Update Template

--- coding-challenges/src/main/java/kae/study/codingchallenges/facebook/Template.java
+++ coding-challenges/src/main/java/kae/study/codingchallenges/facebook/Template.java
@@ -1,34 +1,35 @@
 package kae.study.codingchallenges.facebook;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 /** */
+@SuppressWarnings("DuplicatedCode")
 public class Template {
 
   public static void main(String[] args) throws IOException {
-    try (Scanner scanner = new Scanner(Paths.get(args[0]));
-        PrintWriter writer = getWriter(args)) {
-      int t = scanner.nextInt();
+    long start = System.currentTimeMillis();
+    try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+         PrintWriter writer = getWriter(args)) {
+      int t = Integer.parseInt(reader.readLine());
 
       for (int i = 1; i <= t; i++) {
-        solve(i, scanner, writer);
+        long testCaseStart = System.currentTimeMillis();
+        solve(i, reader, writer);
+        System.out.printf("Case #%02d: %d ms%n", i, System.currentTimeMillis() - testCaseStart);
       }
     }
+    System.out.printf("%nTotal: %d ms%n", System.currentTimeMillis() - start);
   }
 
   private static PrintWriter getWriter(String[] args) throws FileNotFoundException {
-    return args.length > 1
-        ? new PrintWriter(new OutputStreamWriter(new FileOutputStream(args[1])))
-        : new PrintWriter(System.out);
+    return new PrintWriter(new OutputStreamWriter(new FileOutputStream(args[0].replace("input", "output"))));
   }
 
-  static void solve(int caseNumber, Scanner scanner, PrintWriter writer) {
+  private static void solve(int caseNumber, BufferedReader reader, PrintWriter writer) {
     writer.printf("Case #%d: \n", caseNumber);
   }
+
+  static void solve() {}
 }
