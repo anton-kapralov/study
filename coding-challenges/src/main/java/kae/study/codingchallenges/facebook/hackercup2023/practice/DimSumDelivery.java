e3223c28c5e58e7e9860d99cb8a461260c1fde6b
Anton Kapralov <a.e.kapralov@gmail.com>
2023-09-23 11:56:52 -0700

    [facebook/hackercup] Dim Sum Delivery
    https://www.facebook.com/codingcompetitions/hacker-cup/2023/practice-round/problems/B

---
+++ coding-challenges/src/main/java/kae/study/codingchallenges/facebook/hackercup2023/practice/DimSumDelivery.java
@@ -0,0 +1,49 @@
+package kae.study.codingchallenges.facebook.hackercup2023.practice;
+
+import java.io.*;
+
+/**
+ *
+ */
+@SuppressWarnings("DuplicatedCode")
+public class DimSumDelivery {
+
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+             PrintWriter writer = getWriter(args)) {
+            int t = Integer.parseInt(reader.readLine());
+
+            for (int i = 1; i <= t; i++) {
+                long testCaseStart = System.currentTimeMillis();
+                solve(i, reader, writer);
+                System.out.printf("Case #%02d: %d ms%n", i, System.currentTimeMillis() - testCaseStart);
+            }
+        }
+        System.out.printf("%nTotal: %d ms%n", System.currentTimeMillis() - start);
+    }
+
+    private static PrintWriter getWriter(String[] args) throws FileNotFoundException {
+        return new PrintWriter(new OutputStreamWriter(new FileOutputStream(args[0].replace("input", "output"))));
+    }
+
+    private static void solve(int caseNumber, BufferedReader reader, PrintWriter writer) throws IOException {
+        String[] ss = reader.readLine().split(" ");
+        int r = Integer.parseInt(ss[0]);
+        int c = Integer.parseInt(ss[1]);
+        int a = Integer.parseInt(ss[2]);
+        int b = Integer.parseInt(ss[3]);
+        boolean result = solve(r, c, a, b);
+        writer.printf("Case #%d: %s\n", caseNumber, result ? "YES" : "NO");
+    }
+
+    static boolean solve(int r, int c, int a, int b) {
+        if (r <= 2) {
+            return false;
+        }
+        if (c <= 2) {
+            return true;
+        }
+        return r > c;
+    }
+}
---
+++ coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2023/practice/DimSumDeliveryTest.java
@@ -0,0 +1,45 @@
+package kae.study.codingchallenges.facebook.hackercup2023.practice;
+
+import static com.google.common.truth.Truth.assertThat;
+import static kae.study.codingchallenges.facebook.hackercup2023.practice.DimSumDelivery.solve;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+/**
+ *
+ */
+@RunWith(value = Parameterized.class)
+public class DimSumDeliveryTest {
+
+    @Parameter
+    public int r;
+    @Parameter(1)
+    public int c;
+    @Parameter(2)
+    public int a;
+    @Parameter(3)
+    public int b;
+    @Parameter(4)
+    public boolean result;
+
+    @Parameterized.Parameters(name = "Test {index}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+                        {2, 2, 1, 1, false}, // 1
+                        {5, 2, 3, 1, true}, // 2
+                        {4, 4, 3, 3, false}, // 3
+                }
+        );
+    }
+
+    @Test
+    public void solve_returnsExpected() {
+        assertThat(solve(r, c, a, b)).isEqualTo(result);
+    }
+}
