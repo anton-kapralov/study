6bb17ad7b4568901a0d8a7946fba706e0c73e563
Anton Kapralov <a.e.kapralov@gmail.com>
2023-10-01 14:14:31 -0700

    [facebook/hackercup2023] Reformat

--- coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2023/practice/CheeseburgerCorollary2Test.java
+++ coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2023/practice/CheeseburgerCorollary2Test.java
@@ -10,37 +10,36 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 
-/**
- *
- */
+/** */
 @RunWith(value = Parameterized.class)
 public class CheeseburgerCorollary2Test {
 
-    @Parameter
-    public long a;
-    @Parameter(1)
-    public long b;
-    @Parameter(2)
-    public long c;
-    @Parameter(3)
-    public long k;
+  @Parameter public long a;
 
-    @Parameterized.Parameters(name = "Test {index}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(
-                new Object[][]{
-                        {2, 3, 5, 3}, // 1
-                        {2, 3, 2, 1}, // 2
-                        {2, 3, 1, 0}, // 3
-                        {5, 1, 100, 199}, // 4
-                        {1, 3, 100, 100}, // 5
-                        {1, 1, 1000000000000L, 1999999999999L}, // 6
-                }
-        );
-    }
+  @Parameter(1)
+  public long b;
 
-    @Test
-    public void solve_returnsExpected() {
-        assertThat(solve(a, b, c)).isEqualTo(k);
-    }
+  @Parameter(2)
+  public long c;
+
+  @Parameter(3)
+  public long k;
+
+  @Parameterized.Parameters(name = "Test {index}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          {2, 3, 5, 3}, // 1
+          {2, 3, 2, 1}, // 2
+          {2, 3, 1, 0}, // 3
+          {5, 1, 100, 199}, // 4
+          {1, 3, 100, 100}, // 5
+          {1, 1, 1000000000000L, 1999999999999L}, // 6
+        });
+  }
+
+  @Test
+  public void solve_returnsExpected() {
+    assertThat(solve(a, b, c)).isEqualTo(k);
+  }
 }
--- coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2023/practice/DimSumDeliveryTest.java
+++ coding-challenges/src/test/java/kae/study/codingchallenges/facebook/hackercup2023/practice/DimSumDeliveryTest.java
@@ -10,36 +10,36 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 
-/**
- *
- */
+/** */
 @RunWith(value = Parameterized.class)
 public class DimSumDeliveryTest {
 
-    @Parameter
-    public int r;
-    @Parameter(1)
-    public int c;
-    @Parameter(2)
-    public int a;
-    @Parameter(3)
-    public int b;
-    @Parameter(4)
-    public boolean result;
+  @Parameter public int r;
 
-    @Parameterized.Parameters(name = "Test {index}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(
-                new Object[][]{
-                        {2, 2, 1, 1, false}, // 1
-                        {5, 2, 3, 1, true}, // 2
-                        {4, 4, 3, 3, false}, // 3
-                }
-        );
-    }
+  @Parameter(1)
+  public int c;
 
-    @Test
-    public void solve_returnsExpected() {
-        assertThat(solve(r, c, a, b)).isEqualTo(result);
-    }
+  @Parameter(2)
+  public int a;
+
+  @Parameter(3)
+  public int b;
+
+  @Parameter(4)
+  public boolean result;
+
+  @Parameterized.Parameters(name = "Test {index}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          {2, 2, 1, 1, false}, // 1
+          {5, 2, 3, 1, true}, // 2
+          {4, 4, 3, 3, false}, // 3
+        });
+  }
+
+  @Test
+  public void solve_returnsExpected() {
+    assertThat(solve(r, c, a, b)).isEqualTo(result);
+  }
 }
